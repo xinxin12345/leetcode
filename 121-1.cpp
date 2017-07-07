709e91668440452e06ea2fd49c1b5995f6852baa
houxin-PC <18305154989@163.com>
2017-07-07 17:23:54 +0800

    自己写的两个循环，时间复杂度太高，囧......

--- 121-1.cpp
+++ 121-1.cpp
@@ -2,30 +2,20 @@
 #include <vector>
 using namespace std;
 
-#define INT_MAX 0x7fffffff
 
-//int maxProfit(vector<int>& prices){
-//	int m = prices.size();
-//	int maxvalue = 0;
-//	for(int i = 0;i < m-1;i++){
-//		for(int j = i+1;j < m;j++){
-//			if(prices[j] - prices[i] > maxvalue){
-//				maxvalue = prices[j] - prices[i];
-//			}
-//		}	
-//	}
-//	return maxvalue;
-//}
-
-int maxProfit(vector<int> &prices) {
-    int maxPro = 0;
-    int minPrice = INT_MAX;
-    for(int i = 0; i < prices.size(); i++){
-        minPrice = min(minPrice, prices[i]);
-        maxPro = max(maxPro, prices[i] - minPrice);
-    }
-    return maxPro;
+int maxProfit(vector<int>& prices){
+	int m = prices.size();
+	int maxvalue = 0;
+	for(int i = 0;i < m-1;i++){
+		for(int j = i+1;j < m;j++){
+			if(prices[j] - prices[i] > maxvalue){
+				maxvalue = prices[j] - prices[i];
+			}
+		}	
+	}
+	return maxvalue;
 }
+
 
 int main(){
 	int a[6]={7, 1, 5, 3, 6, 4};
